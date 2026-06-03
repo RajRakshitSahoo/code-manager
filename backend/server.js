d81ead604a3f9d3ce21f28fd38d38b2d68edc10b
Raj Rakshit Sahoo <rajrakshit500@gmail.com>
2026-06-03 22:12:58 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-h2hehw.  const express = require('express');
+const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const helmet = require('helmet');
